ddf6065433db050099c3cb73cd07d1540f02af95
Sundar <sundar00500@.com>
2026-07-14 23:40:52 +0530

    chore: update ngrok tunnel URL

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/military': {
-        target: 'https://guidance-glorifier-mango.ngrok-free.dev',
+        target: 'https://concise-egomaniac-starved.ngrok-free.dev',
         changeOrigin: true,
       },
       '/api-manpower': {
